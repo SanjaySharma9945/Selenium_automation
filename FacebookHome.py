3ac5442f8cf7a5cc4c595e5477c50258c5757783
SanjaySharma9945 <126223970+SanjaySharma9945@users.noreply.github.com>
2024-03-13 15:36:06 +0530

    Master Commit

---
+++ FacebookHome.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+
+class FacebookLogin:
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_experimental_option("prefs", {"profile.default_content_setting_values.notifications": 2 })
+        chrome_options.add_argument("--disable-notifications")
+        self.driver = webdriver.Chrome(service=Service(), options=chrome_options)
+        self.driver.maximize_window()
+
+    def login(self, username, password):
+        self.driver.get("https://www.facebook.com/login/")
+        sleep(1)
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login_button()
+        self.Home()
+        sleep(30)
+        self.close_browser()
+
+    def enter_username(self, username):
+        username_field = self.driver.find_element(By.XPATH, '//*[@id="email"]')
+        username_field.send_keys(username)
+        sleep(1)
+
+    def enter_password(self, password):
+        password_field = self.driver.find_element(By.XPATH, '//*[@id="pass"]')
+        password_field.send_keys(password)
+        sleep(1)
+
+    def click_login_button(self):
+        login_button = self.driver.find_element(By.NAME, 'login')
+        login_button.click()
+        
+        
+    def Home(self):
+        home_link = self.driver.find_element(By.XPATH, "//a[@aria-label='Home']")
+        home_link.click()
+        
+    def close_browser(self):
+        self.driver.close()
+        print("Closing the browser")
+
+# Usage
+if __name__ == "__main__":
+    username = "sanjay9448116@gmail.com"
+    password = "Sanj944811@9"
+
+    facebook_login = FacebookLogin()
+    facebook_login.login(username, password)
